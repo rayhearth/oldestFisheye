13bb4d0dbc7846fcd90ce1262354854b21ea9010
Emmanuelle Jupon <emmanuellejupon@gmail.com>
2022-03-10 15:00:52 +0100

    correction factory

--- scripts/factories/_mediaFactory.js
+++ scripts/factories/_mediaFactory.js
@@ -1,5 +1,5 @@
 import { Api } from '../api/_api.js'
-import { VideoMediaCards , ImageMediaCards } from '../models/_mediasCards.js'
+import { VideoMediaCards , ImageMediaCards, lightbox } from '../models/_mediasCards.js'
 
 
 export class MediaFactory extends Api {
@@ -24,7 +24,8 @@ export class MediaFactory extends Api {
           return new VideoMediaCards(media)
         }
       })
-
+      let lightbox1 = new lightbox(medias)
+      let slide = ligthbox.ligthboxSlide
 //instance de la class ligthbox 
 //lorsque nous retournons la methode renderAllMedia on lui transmet notre renderPicture
       return this.renderAllMedia(renderPicture)
@@ -38,6 +39,7 @@ export class MediaFactory extends Api {
     for (let media of medias) {
       all += this.renderOneMedia(media)
     }
+
     //ajout du rendu lightbox slide
     return all
   }
@@ -48,14 +50,14 @@ export class MediaFactory extends Api {
   renderOneMedia(media) {
     // console.log(media)
       return `<article class="mediaContentCard">
-      <div class="mediaCard" media-id=${media._id}>
+      <div class="mediaCard mediaLink" media-id=${media._id}>
       ${media._image?media.renderImage:media.renderVideo}
       </div>
       <div class="media-legend">
         <p class="media-legend-title">${media._title}</p>
         <div class="media-legend-like" liked="no" media-id=${media._id}>
           <span class="media-legend-cunt" id="like-cunt">${media._likes}</span>
-          <svg class="hearth" aria-hidden="true" focusable="false" data-prefix="far" data-icon="heart" role="img"
+          <svg class="hearth" aria-hidden="true" focusable="false" data-prefix="far" data-icon="hearth" role="img"
             xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg="">
             <path fill="currentColor"
               d="M462.1 62.86C438.8 41.92 408.9 31.1 378.7 32c-37.49 0-75.33 15.4-103 43.98l-19.7 20.27l-19.7-20.27C208.6 47.4 170.8 32 133.3 32C103.1 32 73.23 41.93 49.04 62.86c-62.14 53.79-65.25 149.7-9.23 207.6l193.2 199.7C239.4 476.7 247.6 480 255.9 480c8.332 0 16.69-3.267 23.01-9.804l193.1-199.7C528.2 212.5 525.1 116.6 462.1 62.86zM437.6 237.1l-181.6 187.8L74.34 237.1C42.1 203.8 34.46 138.1 80.46 99.15c39.9-34.54 94.59-17.5 121.4 10.17l54.17 55.92l54.16-55.92c26.42-27.27 81.26-44.89 121.4-10.17C477.1 138.6 470.5 203.1 437.6 237.1z">
@@ -64,14 +66,6 @@ export class MediaFactory extends Api {
         </div>
       </div>
     </article>
-    <div class="ligthbox" arial-label="image closeup view" role="dialog">
-    <button class="lightbox__close">close</button>
-    <button class="lightbox__next">next</button>
-    <button class="lightbox__prev">previous</button>
-    <div class="lightbox__container">
-    ${media._image?media.renderImage:media.renderVideo}
-    </div>
-  </div>
     `
   }
 
