aa7ab45064acd42f9370b87396c1caf07f21a556
Emmanuelle Jupon <emmanuellejupon@gmail.com>
2022-04-01 18:22:44 +0200

    light test

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -1,7 +1,6 @@
 export class LightboxContain {
 
   constructor(medias) {
-    // this._id = medias.id
     // this._image = medias.image
     this.currrentMedia = null
     this.listMedias = medias //permet de récupérer tous les médias pour affichage prev et next
@@ -30,30 +29,29 @@ export class LightboxContain {
   openLightbox(){
     
     let content = document.querySelector('#light')
-    this.currentMedia = currentTarget.querySelector()
 
     // console.log(currentMedia)
     document.addEventListener('keyup', this.onKeyUp)
     
-    if(this.currentMedia.image){
-      content.innerHTML=
-      `<img class="picture" src="assets/images/photographers/${this.currentMedia.photographerId}/media/${this.currentMedia.image}" alt="${this.currentMedia.title}">
-      <h2 class="lightbox-media-title" data-lightbox-caption>${this.currentMedia.title}</h2>
-      `
-    }else{
-      content.innerHTML=
-      `<video controls class="picture" media-id=${this.currentMedia.id} aria-label=${this.currentMedia.title}>
-      <source src="assets/images/photographers/${this._photographerId}/media/${this._video}"type="video/mp4">
-      </video>
-      <svg class="svg-inline--fa fa-video mediaIcon" aria-hidden="true" focusable="false" data-prefix="fas"
-      data-icon="video" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" data-fa-i2svg="">
-      <path fill="currentColor"
-      d="M384 112v288c0 26.51-21.49 48-48 48h-288c-26.51 0-48-21.49-48-48v-288c0-26.51 21.49-48 48-48h288C362.5 64 384 85.49 384 112zM576 127.5v256.9c0 25.5-29.17 40.39-50.39 25.79L416 334.7V177.3l109.6-75.56C546.9 87.13 576 102.1 576 127.5z">
-      </path>
-      </svg>
-      <h2 class="lightbox-media-title" data-lightbox-caption>${this.currentMedia.title}</h2>
-      `
-    }
+    // if(this.currentMedia.image){
+    //   content.innerHTML=
+    //   `<img class="picture" src="assets/images/photographers/${this.currentMedia.photographerId}/media/${this.currentMedia.image}" alt="${this.currentMedia.title}">
+    //   <h2 class="lightbox-media-title" data-lightbox-caption>${this.currentMedia.title}</h2>
+    //   `
+    // }else{
+    //   content.innerHTML=
+    //   `<video controls class="picture" media-id=${this.currentMedia.id} aria-label=${this.currentMedia.title}>
+    //   <source src="assets/images/photographers/${this._photographerId}/media/${this._video}"type="video/mp4">
+    //   </video>
+    //   <svg class="svg-inline--fa fa-video mediaIcon" aria-hidden="true" focusable="false" data-prefix="fas"
+    //   data-icon="video" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" data-fa-i2svg="">
+    //   <path fill="currentColor"
+    //   d="M384 112v288c0 26.51-21.49 48-48 48h-288c-26.51 0-48-21.49-48-48v-288c0-26.51 21.49-48 48-48h288C362.5 64 384 85.49 384 112zM576 127.5v256.9c0 25.5-29.17 40.39-50.39 25.79L416 334.7V177.3l109.6-75.56C546.9 87.13 576 102.1 576 127.5z">
+    //   </path>
+    //   </svg>
+    //   <h2 class="lightbox-media-title" data-lightbox-caption>${this.currentMedia.title}</h2>
+    //   `
+    // }
     //on ajoute notre class ligthboxopen
     document.body.classList.add('lightboxOpen')
   }
